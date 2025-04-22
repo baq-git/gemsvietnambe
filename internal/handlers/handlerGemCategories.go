83a8032fed2895abfb86bd5e6e6a4a42e497e291
baq <hoquocbaocyber@gmail.com>
2025-04-22 17:50:56 +0700

    fix: json name convention

--- internal/handlers/handlerGemCategories.go
+++ internal/handlers/handlerGemCategories.go
@@ -10,16 +10,14 @@ import (
 
 type GemCategory struct {
 	ID           uuid.UUID `json:"id"`
-	CategoryName string    `json:"category_name"`
+	CategoryName string    `json:"categoryName"`
 	Slug         string    `json:"slug"`
 	Description  string    `json:"description"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
 }
 
 func (h *Handlers) HandlerGemCategoriesRetrieve(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	dbGemCategories, err := h.DB.GetAllGemCategories(r.Context())
 	if err != nil {
 		logger.Error("Couldn't retrive gem:", err)
